6e7abc3e2fb14734ea2523ab0cb879f074f2f79a
kadek.indra <pratama.2205551056@student.unud.ac.id>
2022-12-26 14:07:47 +0800

    Update main.c.

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "apartment.h"
-void selamat_datang(){
+void perkenalan()
+{
+    printf("\t\t\t\t |= ==================================================================== =|\n");
+    printf("\t\t\t\t ||                      THIS PROGRAM WAS CREATED BY:                    ||\n");
+    printf("\t\t\t\t || ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,, ||\n");
+    printf("\t\t\t\t ||                      I Kadek Indra Agusta Pratama                    ||\n");
+    printf("\t\t\t\t ||                              2205551056                              ||\n");
+    printf("\t\t\t\t || ==================================================================== ||\n");
+    printf("\t\t\t\t ||                       Ni Komang Vaniya Apriandani                    ||\n");
+    printf("\t\t\t\t ||                              2205551019                              ||\n");
+    printf("\t\t\t\t |= ==================================================================== =|\n");
+    system("pause");
+    system("cls");
+}
+void selamat_datang()
+{
     printf("\t\t\t\t|| =============================== ||\n");
     printf("\t\t\t\t||                                 ||\n");
     printf("\t\t\t\t||   Selamat Datang di Apartemen   ||\n");
@@ -10,7 +25,9 @@ void selamat_datang(){
     system("pause");
     system("cls");
 }
-int main(){
+int main()
+{
+    perkenalan();
     selamat_datang();
     mainmenu();
     return 0;
